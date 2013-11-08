3108e482e742b7f51461ae8087eccf002cdf2624
gorkha <anme@odense.dk>
2013-11-08 14:56:26 +0100

    Now using GET for mobile detect instead of cookie

--- mobile_detect.js
+++ mobile_detect.js
@@ -5,24 +5,14 @@
 (function($) {
 
   $.fn.goMobile = function() {
-    setCookie("mobile", true, 365);
     var baseURL =  location.protocol + "//" + location.hostname + location.pathname;
     window.location.href = baseURL+"?m=1"; // time=" + new Date().getTime() + "&
   };
 
 
   $.fn.goDesktop = function() {
-    setCookie("mobile", false, 365);
     var baseURL =  location.protocol + "//" + location.hostname + location.pathname;
     window.location.href = baseURL+"?m=0";
   };
-  
-  function setCookie(c_name, value, exdays)
-  {
-    var exdate = new Date();
-    exdate.setDate(exdate.getDate() + exdays);
-    var c_value = escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
-    document.cookie = c_name + "=" + c_value;
-  }
   
 })(jQuery);
